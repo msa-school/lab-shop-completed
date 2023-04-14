93b0e114556c83445e4f81400013ec385e374a5d
acmexii <apexacme@gmail.com>
2023-04-14 13:13:48 +0000

    updated

--- contract-test/orders/src/main/java/com/example/template/OrderController.java
+++ contract-test/orders/src/main/java/com/example/template/OrderController.java
@@ -1,6 +1,10 @@
 package com.example.template;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +19,16 @@ public class OrderController {
 
     @GetMapping("/order/validateProduct/{productId}")
     public ResponseEntity<String> productStockCheck(@PathVariable(value = "productId") Long productId) {
-
+    
         String productUrl = "http://localhost:8090/product/" + productId;
-        ResponseEntity<String> productEntity = restTemplate.getForEntity(productUrl, String.class);
-
+    
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+    
+        ResponseEntity<String> productEntity = restTemplate.exchange(productUrl, HttpMethod.GET, entity, String.class);
+    
         return productEntity;
     }
+    
 }
