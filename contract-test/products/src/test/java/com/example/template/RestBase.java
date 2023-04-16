1cc8f7f878a4e7bfff25c6e3d1337bf9121e9362
박용주 <apexacme@gmail.com>
2023-04-16 16:57:22 +0900

    Update RestBase.java

--- contract-test/products/src/test/java/com/example/template/RestBase.java
+++ contract-test/products/src/test/java/com/example/template/RestBase.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
@@ -14,7 +13,6 @@ import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@AutoConfigureMessageVerifier
 public class RestBase {
 
     @Autowired
